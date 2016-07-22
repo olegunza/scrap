3176ef229fcf5b5245ca9979b49908b1f39b323f
ozakharov <olegunza@gmail.com>
2016-07-22 16:11:18 +0500

    first

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    get_regions()
+    get_pages()
 
 
-def get_regions():
+def get_pages():
     with requests.Session() as ace:
-        url = 'http://kolesa.kz'
+        url = 'http://kolesa.kz/cars/'
         page = ace.get(url)
-        print(page.text)
+        bs_page = BeautifulSoup(page.text, 'lxml')
+        last_page = bs_page.find('div', {'class': 'pager'})
+        print(last_page)
 
 if __name__ == '__main__':
     main()
